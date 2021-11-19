bc57597a1824d9cef1cf9430fee0dbb0a50656ff
Giordano Agostini <giordano.agostini-extern@rcibanque.com>
2021-11-19 16:58:02 +0100

    TestRepository.java missing

---
+++ rest/src/main/java/com/gago/rest/repositories/TestRepository.java
@@ -0,0 +1,12 @@
+package com.gago.rest.repositories;
+
+import com.gago.rest.entities.Test;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface TestRepository extends CrudRepository<Test, Integer> {
+
+    List<Test> findAll();
+
+}
