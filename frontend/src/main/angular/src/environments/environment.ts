c23a9c6b36779b0693cdf7d44ac1186c214809f8
gagostin <giordano.agostini@yahoo.it>
2023-03-27 14:41:32 +0200

    Removed cmsConfig property from environment files

--- frontend/src/main/angular/src/environments/environment.ts
+++ frontend/src/main/angular/src/environments/environment.ts
@@ -5,9 +5,5 @@ export const environment = {
     realm: 'parent-template',
     url: 'http://localhost:10006/auth'
   },
-  restBaseUrl: 'http://localhost:10002/',
-  cmsConfig: {
-    baseUrl: 'http://localhost:10008/wp-json/wp/v2/',
-    pages: 'pages'
-  }
+  restBaseUrl: 'http://localhost:10002/'
 };
